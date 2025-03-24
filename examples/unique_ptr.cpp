0d50ff97255ca0c1f365a464a28761ce5c0a9aa7
Agaverd <agaverd.seyfaev@gmail.com>
2025-03-24 18:22:46 +0300

    add: added example of unique_ptr pointer use

---
+++ examples/unique_ptr.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <memory>
+
+int main() {
+    for (int i = 0; i < 10; i++) {
+        std::make_unique<int>(10);
+    }
+    return 0;
+}
+
+//указатель unique_ptr уничтожает объект, которым владел, после выхода из scope
